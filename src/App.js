4c8047f573992084c9a84f925727f48f3aa3978d
codewithkevin <oseikelvin709@gmail.com>
2022-06-03 22:37:43 +0000

    update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ import './App.css'
 
 const App = () => {
   return (
-    <nav className="navbar container">
-      <a href="https://github.com/codewithkevin" class="logo">BEAT THE BUZZER</a>
+    <div className="Container">
+      <nav className="navbar container">
+        <a href="https://github.com/codewithkevin" class="logo" title="logo">BEAT THE BUZZER</a>
 
-      <input type="checkbox" id="toggler"></input>
-      <label for="toggler"><i class="ri-menu-line"></i></label>
+        <input type="checkbox" id="toggler"></input>
+        <label for="toggler"><i class="ri-menu-line"></i></label>
 
-      <div className="menu">
-        <Menu />
-      </div>
+        <div className="menu">
+          <Menu />
+        </div>
 
-    </nav>
+      </nav>
+
+
+
+
+
+    </div>  
   );
 };
 
