a07f2bebe3b1443f717fef3753ea13f7c7882b03
codewithkevin <oseikelvin709@gmail.com>
2022-06-04 03:00:01 +0000

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import hero from "./images/hero.jpg";
+import logo from './images/Hero.png';
 
 
 
@@ -12,15 +12,17 @@ const Hero = () => {
         <div className="container">
           <div class="row">
             <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-              <h1>Better Solutions For Your Business</h1>
-              <h2>We are team of talented designers making websites with Bootstrap</h2>
+              <h1>Dog Guards</h1>
+              <h2>We are team of talented dog trainers that trains Dogs.</h2>
+              
               <div class="d-flex justify-content-center justify-content-lg-start">
-                <a href="#about" class="btn-get-started scrollto">Get Started</a>
-                <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+
+                <a href="#about" class="btn-get-started scrollto">Sign Up</a>
+
               </div>
             </div>
-            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="">
-                <img src={hero} alt="Logo" />
+            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                <img src={logo} class="img-fluid animated" alt=""/>
             </div>
           </div>
         </div>
