51dcd0270e1a94c09ddc861ad84d44dcf05b5ad7
codewithkevin <oseikelvin709@gmail.com>
2022-06-04 03:15:22 +0000

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="main-class">
       <nav className="navbar container">
-        <a href="https://github.com/codewithkevin" className="logo" title="logo">BEAT THE BUZZER</a>
+        <a href="https://github.com/codewithkevin" className="logo" title="logo">DOG GUARDS</a>
 
         <input type="checkbox" id="toggler"></input>
         <label for="toggler"><i class="ri-menu-line"></i></label>
