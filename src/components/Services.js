54eebfc6e331c12700ec70f4c90a283860688877
codewithkevin <oseikelvin709@gmail.com>
2022-06-04 12:03:09 +0000

    update

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,51 +5,51 @@ import './Services.css'
 
 const Service = () => {
     return (
-        <section id="services" className="services section-bg">
+        <div id="services" className="services section-bg">
             <div className="container" data-aos="fade-up">
 
                 <div className="section-title">
-                <h2>Services</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                    <h2>Services</h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 </div>
 
                 <div className="row">
-                <div className="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                    <div className="icon-box">
-                    <div className="icon"><i class="bx bxl-dribbble"></i></div>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                    <div className="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                        <div className="icon-box">
+                            <div className="icon"><i class="bx bxl-dribbble"></i></div>
+                            <h4>Lorem Ipsum</h4>
+                            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-                    <div class="icon-box">
-                    <div class="icon"><i class="bx bx-file"></i></div>
-                    <h4>Sed ut perspici</h4>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bx bx-file"></i></div>
+                            <h4>Sed ut perspici</h4>
+                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-                    <div className="icon-box">
-                    <div className="icon"><i class="bx bx-tachometer"></i></div>
-                    <h4>Magni Dolores</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                    <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+                        <div className="icon-box">
+                            <div className="icon"><i class="bx bx-tachometer"></i></div>
+                            <h4>Magni Dolores</h4>
+                            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-                    <div className="icon-box">
-                    <div className="icon"><i class="bx bx-layer"></i></div>
-                    <h4>Nemo Enim</h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                    <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+                        <div className="icon-box">
+                            <div className="icon"><i class="bx bx-layer"></i></div>
+                            <h4>Nemo Enim</h4>
+                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                        </div>
                     </div>
-                </div>
 
                 </div>
 
             </div>
-        </section>
+        </div>
     )
 }
 
