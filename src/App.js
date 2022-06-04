e184a85e96de4e07c8e6c906322b4a53566ad17b
codewithkevin <oseikelvin709@gmail.com>
2022-06-04 01:05:47 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import Button from 'react-bootstrap/Button';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -9,7 +11,7 @@ const App = () => {
   return (
     <div className="Container">
       <nav className="navbar container">
-        <a href="https://github.com/codewithkevin" class="logo" title="logo">BEAT THE BUZZER</a>
+        <a href="https://github.com/codewithkevin" className="logo" title="logo">BEAT THE BUZZER</a>
 
         <input type="checkbox" id="toggler"></input>
         <label for="toggler"><i class="ri-menu-line"></i></label>
